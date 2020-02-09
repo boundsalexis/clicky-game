7fdb2f3edacbe532ea6ad5ba2605ad6ad958ada5
Alexis <lxs.bnds@gmail.com>
2020-02-09 10:13:03 -0800

    fixed routing issues

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <Navbar/>
     <Header/>
     <Wrapper>
-    <Route exact path="/clicky-game" component={Game}/>
+    <Route exact path="*" component={Game}/>
     </Wrapper>
     </Router>
     </ConfigProvider>
